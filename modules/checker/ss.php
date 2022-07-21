a153934e7e87a03d798505a21bd77ed0bf2c6445
MasterMind <87894877+Master-Mind-007@users.noreply.github.com>
2022-07-21 09:26:34 +0530

    Update ss.php

--- modules/checker/ss.php
+++ modules/checker/ss.php
@@ -204,14 +204,14 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
                 'text'=>"<b>⚜️ CC -»</b> <code>$lista</code>
 <b>⚜️ 𝑺𝒕𝒂𝒕𝒖𝒔 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅 ✅
 <b>⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» 𝒔𝒕𝒂𝒕𝒖𝒔 = 𝒔𝒖𝒄𝒄𝒆𝒆𝒅𝒆𝒅 
-<b>⚜️ 𝑮𝑨𝑻𝑬 -» WateCharity.org ♻️
+<b>⚜️ 𝑮𝑨𝑻𝑬 -» WateCharity ♻️
 <b>⚜️ 𝑩𝒂𝒏𝒌:</b> $bank
 <b>⚜️ 𝑪𝒐𝒖𝒏𝒕𝒓𝒚:</b> $cname ($emoji - 💲$currency)
 <b>⚜️ 𝑰𝒏𝒇𝒐: CREDIT - $schemename - $typename
 <b>⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» $result2
 <b>⚜️ 𝑻𝒊𝒎𝒆 𝑺𝒑𝒆𝒏𝒕 -» <b>$time</b><b>s</b>
 <b>⚜️ 𝑪𝒉𝒆𝒄𝒌𝒆𝒅 𝑩𝒚: <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>⚜️ 𝑶𝒘𝒏𝒆𝒓: <a href='t.me/MasterMind_Mikhil'>╰ 𝔪Ⓐᔕţ𝒆ℝᵐίⓃᗪ ╮</a></b>",
+<b>⚜️ 𝑶𝒘𝒏𝒆𝒓: <a href='t.me/MasterMind_Mikhil'>MasterMind</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
@@ -231,7 +231,7 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
 <b>⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» $result2
 <b>⚜️ 𝑻𝒊𝒎𝒆 𝑺𝒑𝒆𝒏𝒕 -» <b>$time</b><b>s</b>
 <b>⚜️ 𝑪𝒉𝒆𝒄𝒌𝒆𝒅 𝑩𝒚: <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>⚜️ 𝑶𝒘𝒏𝒆𝒓: <a href='t.me/MasterMind_Mikhil'>╰ 𝔪Ⓐᔕţ𝒆ℝᵐίⓃᗪ ╮</a></b>",
+<b>⚜️ 𝑶𝒘𝒏𝒆𝒓: <a href='t.me/MasterMind_Mikhil'>MasterMind</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
@@ -252,7 +252,7 @@ if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){
 <b>⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» $result2
 <b>⚜️ 𝑻𝒊𝒎𝒆 𝑺𝒑𝒆𝒏𝒕 -» <b>$time</b><b>s</b>
 <b>⚜️ 𝑪𝒉𝒆𝒄𝒌𝒆𝒅 𝑩𝒚: <a href='tg://user?id=$userId'>$firstname</a></b>
-<b>⚜️ 𝑶𝒘𝒏𝒆𝒓: <a href='t.me/MasterMind_Mikhil'>╰ 𝔪Ⓐᔕţ𝒆ℝᵐίⓃᗪ ╮</a></b>",
+<b>⚜️ 𝑶𝒘𝒏𝒆𝒓: <a href='t.me/MasterMind_Mikhil'>MasterMind</a></b>",
                 'parse_mode'=>'html',
                 'disable_web_page_preview'=>'true'
                 
